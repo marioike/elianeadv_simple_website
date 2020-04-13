d84df315454a305846cd3e008efa05bf1a221cfc
Mario Ikeno Junior <marioike@gmail.com>
2020-04-12 22:48:43 -0300

    prod008

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import * as AOS from 'aos';
 
 
@@ -7,21 +8,34 @@ import * as AOS from 'aos';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+
+
+
+
+export class AppComponent {
   Title = 'Eliane-Advogada';
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        (<any>window).ga('set', 'page', event.urlAfterRedirects);
+        (<any>window).ga('send', 'pageview');
+      }
+    });
+  }
 
   ngOnInit(): void {
 
     AOS.init();
-  
+
   }
 
   onActivate(event) {
-    window.scroll(0,0);
+    window.scroll(0, 0);
     //or document.body.scrollTop = 0;
     //or document.querySelector('body').scrollTo(0,0)
-    
-}
+
+  }
 
 }
 
